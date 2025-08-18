8ea8be9469afe8d83fc0a14ad1d33409c79ebb49
flook25 <flook11223344@gmail.com>
2025-08-18 14:58:45 +0700

    save

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -5,7 +5,15 @@ const prisma = new PrismaClient();
 export const ProductController = {
   create: async (req, res) => {
     try {
-      await prisma.product.create({
+      const qty = req.body.qty
+
+      if (qty > 1000) {
+        res.status(400).json({ message: "Quantity exceeds limit of 1,000" });
+        return;
+      }
+
+      for (let i = 0; i < qty; i++) {
+        await prisma.product.create({
         data: {
           name: req.body.name,
           release: req.body.release,
@@ -15,9 +23,13 @@ export const ProductController = {
           customerPhone: req.body.customerPhone ?? '',
           customerAddress: req.body.customerAddress ?? '',
           remarks: req.body.remarks ?? '', // เปลี่ยนจาก remark เป็น remarks
+          serial: req.body.serial ?? '', // เพิ่ม serial
           status: 'instock'
         },
       });
+        
+      }
+      
       res.status(201).json({ message: "Product created successfully" });
     } catch (error) {
       console.error('Create Product error:', error);
@@ -30,10 +42,36 @@ export const ProductController = {
         orderBy: { 
           id : 'desc' 
         },
+        where: {
+          status: "instock" // แสดงเฉพาะสินค้าที่มีสถานะ instock
+        }
       });
       res.json(products);
     } catch (error) {
       res.status(500).json({ message: error.message });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { id, ...data } = req.body;
+      await prisma.product.update({
+        where: { id: String(req.params.id) },
+        data,
+      });
+      res.json({ message: "Product updated successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  remove: async (req, res) => {
+    try {
+      await prisma.product.update({
+        where: { id: String(req.params.id) },
+        data: { status: "delete"} // แปลง id เป็น String
+      });
+      res.json({ message: "Product deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message}  )
     }
   }
 };
